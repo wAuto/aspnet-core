d68fdc812c7ad6de8033a7405a242301fcfc72c1
step mr <ktop3256@163.com>
2020-11-17 21:19:28 +0800

    添加项目文件。

---
+++ src/ABP.Test.Web.Host/DomainEntities/Goods.cs
@@ -0,0 +1,23 @@
+﻿using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ABP.Test.Web.Host.DomainEntities
+{
+    [Table("Goods")]
+    public class Goods:AuditedEntity<long>
+    {
+        /// <summary>
+        /// 商品名
+        /// </summary>
+        [Column(TypeName = "text")]
+        public string Name { get; set; }
+        /// <summary>
+        /// 价格
+        /// </summary>
+        public int Price { get; set; }
+    }
+}
